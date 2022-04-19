e06504f727f0a30de195da62c7466718181cfcc1
Ajmal Orawala <Ajuba@Macju.local>
2022-04-19 17:31:48 +0530

    feat:Add TableView Header

--- Bankey/Bankey/AccountSummaryViewController.swift
+++ Bankey/Bankey/AccountSummaryViewController.swift
@@ -29,6 +29,7 @@ class AccountSummaryViewController: UIViewController {
 extension AccountSummaryViewController {
     private func setup() {
         setupTableView()
+        setupTableHeaderView()
     }
     
     private func setupTableView() {
@@ -45,6 +46,16 @@ extension AccountSummaryViewController {
             tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
         ])
     }
+    
+    private func setupTableHeaderView() {
+        let header = AccountSummaryHeaderView(frame: .zero)
+        
+        var size = header.systemLayoutSizeFitting(UIView.layoutFittingCompressedSize)
+        size.width = UIScreen.main.bounds.width
+        header.frame.size = size
+        
+        tableView.tableHeaderView = header
+    }        
 }
 
 extension AccountSummaryViewController: UITableViewDataSource {
