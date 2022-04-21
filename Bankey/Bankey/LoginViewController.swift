295cbf1265965a9bcc02ba647193d56b96010e75
Ajmal Orawala <Ajuba@Macju.local>
2022-04-21 13:49:18 +0530

    feat: Shake Sign in Btn

--- Bankey/Bankey/LoginViewController.swift
+++ Bankey/Bankey/LoginViewController.swift
@@ -174,6 +174,17 @@ extension LoginViewController {
     private func configureView(withMessage message: String) {
         errorMessageLabel.isHidden = false
         errorMessageLabel.text = message
+        shakeButton()
+    }
+    private func shakeButton(){
+        let animation = CAKeyframeAnimation()
+        animation.keyPath = "position.x"
+        animation.values = [0,10,-10,10,0]
+        animation.keyTimes = [0,0.16,0.5,0.83,1]
+        animation.duration = 0.4
+        
+        animation.isAdditive = true
+        signInButton.layer.add(animation, forKey: "shake")
     }
 }
 
@@ -201,6 +212,5 @@ extension LoginViewController {
             self.view.layoutIfNeeded()
         }
         animator3.startAnimation(afterDelay: 0.7)
-    
     }
 }
